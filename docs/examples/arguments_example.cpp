2dd877c5c99e4604f139bf3e3c3aaf8908587fa5
Erwan BERNARD <bernard.erwan@gmail.com>
2018-07-10 19:18:36 +0200

    example:  arguments_example

---
+++ docs/examples/arguments_example.cpp
@@ -0,0 +1,68 @@
+#include "clara.hpp"
+#include <iostream>
+#include <string>
+
+// usage:
+//   clara-arguments_example [<files> ... ] options
+
+// where options are:
+//   -?, -h, --help       display usage information
+//   -v, --verbose        verbose mode
+//   -q, --quiet          quiet mode
+//   -r, --make-report    make report
+
+// for convenience
+using namespace clara;
+
+struct Clo {
+  bool verbose;
+  bool quiet;
+  bool report;
+  std::vector<std::string> files;
+};
+
+int main(int argc, const char *argv[]) {
+
+  Clo options;
+  bool showHelp = false;
+
+  // clang-format off
+    auto cli
+        = Help(showHelp)
+        | Opt(options.verbose)
+            ["-v"]["--verbose"]
+            ("verbose mode")
+        | Opt(options.quiet)
+            ["-q"]["--quiet"]
+            ("quiet mode")
+        | Opt(options.report)
+            ["-r"]["--make-report"]
+            ("make report")
+        | Arg(options.files, "files")
+            ("optional input file");
+  // clang-format on
+
+  auto result = cli.parse(Args(argc, argv));
+
+  if (!result) {
+    std::cerr << "Error in command line: " << result.errorMessage() << std::endl;
+    exit(1);
+  }
+  if (showHelp) {
+    std::cout << cli << std::endl;
+    exit(1);
+  }
+
+  std::cout << "===================================================" << std::endl;
+  std::cout << "   verbose : " << options.verbose << std::endl;
+  std::cout << "   quiet   : " << options.quiet << std::endl;
+  std::cout << "   report  : " << options.report << std::endl;
+  std::cout << "   files   : ";
+  for (auto &&i : options.files) {
+    std::cout << i << "  ";
+  }
+  std::cout << std::endl;
+  std::cout << "===================================================" << std::endl;
+
+  return 1;
+}
